82b0dba94f7c78f9927b65b507d7c9485faf76ea
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-12-04 21:30:28 -0800

    Add support for line_chart

--- functions.php
+++ functions.php
@@ -114,7 +114,69 @@ function visualization_bar_chart_shortcode( $atts, $content = null ) {
 //Generate a line chart
 
 function visualization_line_chart_shortcode( $atts, $content = null ) {
-    return "Unimplemented";
+    //use global variables
+    global $graph_ids;
+
+    $options = shortcode_atts( array(
+            'width' => "400px",
+            'height' => "300px",
+            'title' => "Graph",
+            
+            //By default the axis titles are empty
+            'h_title' => "",
+            'v_title' => "",
+            
+            //By default give iterated id to the graph
+            'id' => "graph_id" + count($graph_ids),
+        ), $atts );
+
+    //Register the graph ID
+    $graph_ids[] = $options['id'];
+
+    $graph_content = "";
+
+    //Generate the div
+    $graph_content .= visualization_new_div($options['id'], $options['width'], $options['height']);
+
+    //Generate the Javascript for the graph
+    $graph_draw_js = "";
+
+    $graph_draw_js .= '<script type="text/javascript">';
+    $graph_draw_js .= 'function draw_' . $options['id'] . '(){';
+
+    $graph_draw_js .= 'var data = google.visualization.arrayToDataTable([';
+    $graph_draw_js .= str_replace(array('<br/>', '<br />'), '', $content);
+    $graph_draw_js .= ']);';
+
+    $graph_draw_js .= 'var graph = new google.visualization.LineChart(document.getElementById(\'' . $options['id'] . '\'));';
+
+    $graph_draw_js .= 'var options = {';
+    $graph_draw_js .= 'curveType: "function", ';
+    $graph_draw_js .= 'animation: {duration: 1200, easing:"in"}, ';
+    $graph_draw_js .= 'title:"' . $options['title'] . '",';
+    $graph_draw_js .= 'width:\'' . $options['width'] . '\',';
+    $graph_draw_js .= 'height:\'' . $options['height'] . '\',';
+
+    if(!empty($options['h_title'])){
+        $graph_draw_js .= 'hAxis: {title: "' . $options['h_title'] . '", slantedText:true},';
+    }
+
+    if(!empty($options['v_title'])){
+        $graph_draw_js .= 'vAxis: {title: "' . $options['v_title'] . '", viewWindow: {min:0}}';
+    } else {
+        $graph_draw_js .= 'vAxis: {viewWindow: {min:0}}';
+    }
+
+    $graph_draw_js .= '};';
+    
+    $graph_draw_js .= 'graph.draw(data, options);';
+
+    $graph_draw_js .= '}';
+    $graph_draw_js .= '</script>';
+
+    $graph_content .= $graph_draw_js;
+
+    return $graph_content;
 }
 
 //Filter to add JS to load all the graphs previously entered as shortcodes
@@ -123,7 +185,7 @@ function visualization_load_graphs_js($content) {
     //use global variables
     global $graph_ids;
 
-    if(is_single()) {
+    if(is_single() && count($graph_ids) > 0) {
         $graph_draw_js = "";
         
         $graph_draw_js .= '<script type="text/javascript">';
